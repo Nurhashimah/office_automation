df17ec74047e06221742bba0104dc27fd673478f
Nurhashimah <nurhashimah77@gmail.com>
2013-02-07 18:47:23 +0800

    Add-in postcode into address book (db not affected)

--- app/models/addbook.rb
+++ app/models/addbook.rb
@@ -5,6 +5,26 @@ class Addbook < ActiveRecord::Base
   has_many :ptcourses,                            :foreign_key => 'provider_id'
   has_many :maintainers,  :class_name => 'Maint', :foreign_key => 'maintainer_id'
   
+  # start of-----7Feb2013 - add-in postcode
+  attr_accessor :postcode
+  
+  before_save :save_my_vars
+  
+  def save_my_vars
+    self.address = address.to_s + "//"+postcode.to_s
+  end
+  
+  def postcode
+    #@postcode 
+    if @addbook
+      @addbook.address.split("//").last
+    else
+      
+      @postcode
+    end
+  end
+  # end of-----7Feb2013 - add-in postcode
+  
   def self.search(search)
     if search
       find(:all, :conditions => ['name ILIKE ?', "#{search}%"])
